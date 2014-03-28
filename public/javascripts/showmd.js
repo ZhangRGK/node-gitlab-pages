ca9d1a9ebd3eada09686a578e42e4eb9ef1f86b3
zhangrui <stamen0630@gmail.com>
2014-03-28 09:18:03 +0800

    增加md文件导航

--- public/javascripts/showmd.js
+++ public/javascripts/showmd.js
@@ -5,11 +5,41 @@ $(function() {
     }();
 
     var createnav = function() {
-        var navString = "";
-        $("h1,h2").each(function() {
-            if($(this).attr("tagName") == "h1") {
-
+        var navtree = [];
+        $("h2,h3").each(function(index) {
+            var node = $(this);
+            if(node[0].tagName == "H2") {
+                navtree.push({"index":index,"html":node.html(),"children":[]});
+            } else {
+                if(navtree.length ==0 ){
+                    navtree.push({"index":-1,"html":"root","children":[]})
+                }
+                navtree[navtree.length-1].children.push({"index":index,"html":node.html()});
             }
+            $(this).after("<a name='"+index+"'></a>");
         });
+
+        var navstr = "";
+        for(var i in navtree) {
+            var h2 = navtree[i];
+            navstr += "<li><a href='#"+h2.index+"'>"+h2.html+"</a>";
+            if(h2.children) {
+                navstr += "<ul class='nav'>";
+                for(var j in h2.children) {
+                    var h3 = h2.children[j];
+                    navstr += "<li><a href='#"+h3.index+"'>"+h3.html+"</a></li>"
+                }
+                navstr += "</ul>"
+            }
+            navstr += "</li>";
+        }
+        $("#tree").html(navstr);
     }();
+
+    $("li>a").on("click",function() {
+        $(".active").removeClass("active");
+        $(this).addClass("active");
+        $(".show").removeClass("show");
+        $(this).next("ul").addClass("show");
+    });
 });
--- views/blogs/zr/index.md
+++ views/blogs/zr/index.md
@@ -1,4 +1,63 @@
 # Index
 test for markdown
-
+## test code
+### t1
+### t2
     var foo = {a:"1",b:"2"};
+## test nothing
+### n1
+### n2
+# Index
+test for markdown
+## test code
+### t1
+### t2
+    var foo = {a:"1",b:"2"};
+## test nothing
+### n1
+### n2
+# Index
+test for markdown
+## test code
+### t1
+### t2
+    var foo = {a:"1",b:"2"};
+## test nothing
+### n1
+### n2
+# Index
+test for markdown
+## test code
+### t1
+### t2
+    var foo = {a:"1",b:"2"};
+## test nothing
+### n1
+### n2
+# Index
+test for markdown
+## test code
+### t1
+### t2
+    var foo = {a:"1",b:"2"};
+## test nothing
+### n1
+### n2
+# Index
+test for markdown
+## test code
+### t1
+### t2
+    var foo = {a:"1",b:"2"};
+## test nothing
+### n1
+### n2
+# Index
+test for markdown
+## test code
+### t1
+### t2
+    var foo = {a:"1",b:"2"};
+## test nothing
+### n1
+### n2
